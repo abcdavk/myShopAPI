43fdc070ea7506bd797bf942e788378734b9f14d
abcdavk <xloot.mcpedl@gmail.com>
2023-10-27 19:07:58 +0700

    Update

--- src/scripts/SHOP_conf.js
+++ src/scripts/SHOP_conf.js
@@ -1,19 +1,3 @@
-/*
-         ____  _
- _ __ ___ / ___|| |
-| '_ ` _ \\___ \| |
-| | | | | |___) | |___
-|_| |_| |_|____/|_____|
-
-                  __ _                       _   _
-  ___ ___  _ __  / _(_) __ _ _   _ _ __ __ _| |_(_) ___  _ __
- / __/ _ \| '_ \| |_| |/ _` | | | | '__/ _` | __| |/ _ \| '_ \
-| (_| (_) | | | |  _| | (_| | |_| | | | (_| | |_| | (_) | | | |
- \___\___/|_| |_|_| |_|\__, |\__,_|_|  \__,_|\__|_|\___/|_| |_|
-                       |___/
-*/
-
-// VERSION 2.1.0
 export const SHOP = {
   commands: [".s", ".sh", ".shop", ".st", ".store"], // Command aliases, you can add more.
   currency: ["dollar", "$", 10], // Scroreboard objectives, Symbols, Default first player money amount
@@ -37,28 +21,21 @@ export const SHOP = {
     no_icon_test: {
       icon: "",
       items: [
-        ["minecraft:dirt", "", 10, 2],
-        ["minecraft:stone", "", 15, 3],
-        ["minecraft:gravel", "", 20, 4]
+        ["minecraft:dirt", "", 10, 5],
+        ["minecraft:stone", "", 15, 7],
+        ["minecraft:gravel", "", 20, 10],
+        ["minecraft:diamond", "", 50, 25]
       ]
-    }
-  }
-};
-
-
-// VERSION: 2.0.1
-/*
-export const SHOP = {
-  commands: [".", "shop"], // Prefix, Suffix
-  currency: ["dollar", "$", 10], // Scroreboard objectives, Symbols, Default first player money amount
-  categories: {
-    example: {
+    },	
+     items: {
+      icon: "",
       items: [
-        ["minecraft:dirt", 10, "textures/blocks/dirt"],
-        ["minecraft:stone", 15, "textures/blocks/stone"],
-        ["minecraft:gravel", 20, "textures/blocks/gravel"]
+        ["minecraft:wooden_sword", "", 10, 5],
+        ["minecraft:stone", "", 15, 7],
+        ["minecraft:gravel", "", 20, 10],
+        ["minecraft:diamond", "", 50, 25]
       ]
     }
   }
 };
-*/
+
